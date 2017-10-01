0b89de80ed4f28e9dc2809be31225a1c99e2e104
zjian <zjian@stanford.edu>
2017-10-01 16:26:23 -0700

    logging code ready for run

--- numerical_test/run_test_master.sh
+++ numerical_test/run_test_master.sh
@@ -1,1 +1,1 @@
-python YF_batch_50-Oct-1-test-master.py --seed=1 --nhidden=1000  --init_range=0.1  --log_dir=./results/full_master_test_seed_1 --use_cuda
+python YF_batch_50-Oct-1-test-master.py --seed=1 --nhidden=1000  --init_range=0.1  --log_dir=./results/full_master_test_seed_1 --cuda --debug 
