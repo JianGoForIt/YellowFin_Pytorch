456aacf969917dcebd451090276011f00b646513
Jian Zhang <zhangjianthu@gmail.com>
2017-10-05 00:21:37 -0700

    test adaptive thresholding with hmax

--- pytorch-cifar/run.sh
+++ pytorch-cifar/run.sh
@@ -1,1 +1,1 @@
-python main.py --lr=1.0 --mu=0.0 --logdir=./results/YF_2.0_seed_lr_grad_clamp_0.1 --opt_method=YF --seed=1 --lr_thresh=0.1
+python main.py --lr=1.0 --mu=0.0 --logdir=./results/YF_2.0_seed_lr_grad_clamp_1.0_linear_min --opt_method=YF --seed=1 --lr_thresh=1.0
