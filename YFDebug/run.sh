3d6d69e3d1f308d1c0d3465f3bbe4a3aeff2427d
Jian Zhang <zhangjianthu@gmail.com>
2017-10-18 21:17:14 -0700

    for clipping motivation plotting

--- YFDebug/run.sh
+++ YFDebug/run.sh
@@ -1,1 +1,1 @@
-python train_mnist_torch.py --log_dir='./results/seed_2_h_max_log_lr_t_mu_t_test_spike_warm_start_clip_h_max_100_hard_geo_mean_compare_clean_up' --fast_bound_const=1.0 --seed=2
+python train_mnist_torch.py --log_dir='./results/seed_2_h_max_log_clip_h_max_100_hard_plot_with_clipping' --seed=2
