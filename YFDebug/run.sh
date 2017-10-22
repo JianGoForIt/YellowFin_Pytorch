d54f50c5b330eaf1d5f90a1f11b16b0ac847df2d
Jian Zhang <zhangjianthu@gmail.com>
2017-10-22 00:18:17 -0700

    100 seed YF Debug run late commit

--- YFDebug/run.sh
+++ YFDebug/run.sh
@@ -1,1 +1,4 @@
-python train_mnist_torch.py --log_dir='./results/seed_2_h_max_log_lr_t_mu_t_test_spike_warm_start_clip_h_max_100_hard_geo_mean_compare_clean_up' --fast_bound_const=1.0 --seed=2
+for ((seed=1;seed<=100;seed++));
+do
+  python train_mnist_torch.py --log_dir='./results_stress/seed_${seed}_h_max_log_h_max_clip_100_hard_stress_test' --fast_bound_const=1.0 --seed=${seed}
+done
