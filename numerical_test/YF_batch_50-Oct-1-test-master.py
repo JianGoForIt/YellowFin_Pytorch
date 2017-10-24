b4f29a1f451670a6c5236cdd47369f0917e6dd44
zjian <zjian@stanford.edu>
2017-10-24 13:34:11 -0700

    update checkpointing test scripts

--- numerical_test/YF_batch_50-Oct-1-test-master.py
+++ numerical_test/YF_batch_50-Oct-1-test-master.py
@@ -232,9 +232,9 @@ def train_early_stopping(mini_batch_size, X_train, X_train_feature, y_train, X_t
 
 #             optimizer._lr = 0.5
 
-	    if i > 1:	    
-              fast_view_act_list.append(4.0/( (math.sqrt(optimizer._global_state["grad_norm_squared"] ) + math.sqrt(optimizer._h_min) )**2 + 1e-6))
-              lr_grad_norm_clamp_act_list.append(optimizer._lr_grad_norm_thresh / (math.sqrt(optimizer._global_state["grad_norm_squared"] ) + 1e-6) )
+	    #if i > 1:	    
+            #  fast_view_act_list.append(4.0/( (math.sqrt(optimizer._global_state["grad_norm_squared"] ) + math.sqrt(optimizer._h_min) )**2 + 1e-6))
+            #  lr_grad_norm_clamp_act_list.append(optimizer._lr_grad_norm_thresh / (math.sqrt(optimizer._global_state["grad_norm_squared"] ) + 1e-6) )
 
             loss, grad_norm_250 = train_data(tokens, features, labels, word_attn_model, sent_attn_model, optimizer, loss_criterion, cuda=use_cuda, lstm=args.use_lstm)
             loss_s, gn_s = loss, grad_norm_250
