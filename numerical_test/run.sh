5c0fca628a472dd6f0d39ddca6ee3e17f52883e4
zjian <zjian@stanford.edu>
2017-10-24 12:55:12 -0700

    stress test for checkpointing feature

--- numerical_test/run.sh
+++ numerical_test/run.sh
@@ -1,3 +1,2 @@
-#bash run_test_master.sh 2 0.2 2000 0.5
-bash run_test_master.sh 1 0.1 1000 2.0
-#bash run_test_master.sh 2 0.2 1000 5.0 
+bash run_test_ckpt_disk.sh 1 0.1 50 2.0
+bash run_test_ckpt_memory.sh 1 0.1 50 2.0
---
+++ numerical_test/run_test_ckpt_disk.sh
@@ -0,0 +1,5 @@
+echo seed ${1}
+echo init range ${2}
+echo nhidden ${3}
+echo lr clamp ${4}
+python YF_batch_50-Oct-1-test-ckpt-disk.py --seed=${1} --nhidden=${3}  --init_range=${2} --lr_thresh=${4} --log_dir=./results/seed_${1}_init_${2}_nhid_${3}_test_disk_ckpt --debug  --use_cuda --use_lstm
