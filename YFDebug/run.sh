851e357f1fdd3ae4a8d195fbb5ae2782deba722c
Jian Zhang <zhangjianthu@gmail.com>
2017-10-24 17:09:06 -0700

    test run scripts for Olexa's model

--- YFDebug/run.sh
+++ YFDebug/run.sh
@@ -1,1 +1,1 @@
-python train_mnist_torch.py --log_dir='./results/seed_2_h_max_log_clip_h_max_100_hard_plot_with_clipping' --seed=2
+python train_mnist_torch.py --log_dir='./results/seed_5_test_memory_ckpt' --seed=5
